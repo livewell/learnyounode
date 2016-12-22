f7fd375624cbbe1d48f3c968dcc0756b06a0c42b
Kory Le <koryqle@gmail.com>
2016-12-22 15:04:22 -0800

    finished firstio.js

--- firstio.js
+++ firstio.js
@@ -1,2 +1,7 @@
 var fs = require('fs');
-console.log('hi');
+
+var file = fs.readFileSync(process.argv[2]).toString();
+var fileByLine = file.split("\n");
+var numOfLines = fileByLine.length-1;
+
+console.log(numOfLines);
