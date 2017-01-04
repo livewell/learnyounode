a8522a771a62c7cfc739e6c9b0a06f2a0b73ec24
Kory Le <drinkwater@korys-mbp.attlocal.net>
2017-01-03 21:44:30 -0800

    finished make it modular

--- filteredls.js
+++ filteredls.js
@@ -1,18 +1,23 @@
 var fs = require('fs');
 var path = require('path');
 
-var filteredLS = function (folder, givenExt, callback) {
+module.exports = function (folder, givenExt, callback) {
+    //array to store files found in LS
+    var foundFiles = [];
+
+
+    //reads a directory and return list of files
     fs.readdir(folder, function (err, files) {
-        if (err) return console.log(err);
+        if (err) return callback(err);
 
+        //sorts out files by given extension
         files.forEach(function (file) {
-            if (path.extname(file) === givenExt) {
-                console.log(file);
+            this.givenExt = "." + givenExt;
+            if (path.extname(file) === this.givenExt) {
+                foundFiles.push(file);
             }
-        });
-
-        callback(err,files);
+        }); 
+        
+        return callback(null, foundFiles);
     });
 }
-
-exports.filteredLS = filteredLS;
--- process.js
+++ process.js
@@ -2,9 +2,9 @@ var ls = require('./filteredls.js');
 
 //variables to pass to module
 var folder = process.argv[2];
-var givenExt = "." + process.argv[3];
+var givenExt = process.argv[3];
 
-ls.filteredLS(folder, givenExt, function(err, files){
+ls(folder, givenExt, function(err, files){
     if (err) return console.log(err);
 
     files.forEach(function(file){
