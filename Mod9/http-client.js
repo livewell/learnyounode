355259eee502e1046f7320806df8e62dec0cf82e
Kory Le <drinkwater@korys-mbp.ptc.ca.kp.org>
2017-01-12 16:03:02 -0800

    finished module 9

--- Mod9/http-client.js
+++ Mod9/http-client.js
@@ -2,7 +2,27 @@ var http = require('http');
 var bl = require('bl');
 const async = require('async');
 
-
+// list of urls
 var urls = [process.argv[2], process.argv[3], process.argv[4]];
 
-async.map
+async.map(urls, httpGet, function(err, data){
+    if (err) return console.log(err);
+
+    data.forEach(function(line){
+        console.log(line);
+    })
+});
+
+function httpGet(url, callback){
+    http.get(url, function(response){
+        response.pipe(bl(function(err,data){
+            if (err) 
+                return console.log(err);
+        
+            data = data.toString();
+            callback(null,data);
+        }))
+    })
+};
+
+
